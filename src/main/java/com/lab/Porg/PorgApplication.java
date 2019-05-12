c1fe31b2d895667f1979d850cfa1244352a30926
Luis  Senior <heybluguy@yahoo.com>
2019-05-11 19:13:17 -0500

    Initial commit.

---
+++ src/main/java/com/lab/Porg/PorgApplication.java
@@ -0,0 +1,15 @@
+package com.lab.Porg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PorgApplication {
+
+	public static void main(String[] args) {
+
+//	SpringApplication.run(PorgApplication.class, args)
+		System.out.println("Hello World");
+	}
+
+}
